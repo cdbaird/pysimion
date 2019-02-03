f3ea98f2e503390c9cf5d4ebe141142078f6dc1c
cdbaird <bairdc13@gmail.com>
2019-02-03 17:52:49 +0000

    Add methods to Fly and Particle classes

--- fly_parser.py
+++ fly_parser.py
@@ -3,21 +3,48 @@ import os
 import csv
 import pandas as pd 
 
-from particle import Particle
+from .particle import Particle
 
 class Fly():
 
 	cols = ["Ion N","Events","X","Y","Z","Vt","Azm","Elv","B","Bx","By","Bz","KE", "KE Error"]
+	dtypes = {"Ion N" : str,
+			"Events" : str,
+			"X" : 'float64',
+			"Y" : 'float64',
+			"Z" : 'float64',
+			"Vt" : 'float64',
+			"Azm" : 'float64',
+			"Elv" : 'float64',
+			"B" : 'float64',
+			"Bx" : 'float64',
+			"By" : 'float64',
+			"Bz" : 'float64',
+			"KE" : 'float64',
+			"KE Error" : 'float64'}
 
-	def __init__(self, fn):
+
+	def __init__(self, fn, zero_origin=True):
 		self.fn = fn
-		self.df = pd.read_csv(fn, header=6, usecols=self.get_cols())
-		self.df.rename(index=str, columns={"Ion N": "Num", "KE Error": "Err"}, inplace=True)
-		self.df.dropna(inplace=True)
-		self.df['Num'] = pd.to_numeric(self.df['Num'], downcast='integer')
-		self.assign_particles()
+		self.df = pd.read_csv(fn, header=6, 
+						usecols=self.get_cols(), 
+						dtype=self.get_dtypes())
 
-	def assign_particles(self):
+		self.df.rename(index=str,
+						columns={"Ion N": "Num", "KE Error": "Err"},
+						inplace=True)
+
+		self.df.dropna(inplace=True)
+
+		self.df['Num'] = pd.to_numeric(self.df['Num'], 
+						downcast='integer')
+
+		self.df['Events'] = pd.to_numeric(self.df['Events'], 
+						downcast='integer')
+
+		self.__assign_particles()
+
+	def __assign_particles(self):
 		part_ids = np.unique(self.df['Num'])
 		particles = np.empty(len(part_ids), dtype=object)
 		for i, n in enumerate(part_ids):
@@ -26,13 +53,40 @@ class Fly():
 
 		self.particles = particles
 
+	def get_splats(self):
+		s = []
+		for p in self.particles:
+			s.append(p.splat)
+
+		s = np.array(s)
+		return s
+
+	def get_ke(self):
+		ke = []
+		for p in self.particles:
+			ke.append(p.energy)
+
+		ke = np.array(ke)
+		return ke
+
+	def get_traj(self):
+		xyz = []
+		for p in self.particles:
+			xyz.append(p.xyz)
+
+		xyz = np.array(xyz)
+		return xyz
+
 	@classmethod
 	def get_cols(cls):
 		return cls.cols
 
+	@classmethod
+	def get_dtypes(cls):
+		return cls.dtypes
+
 if __name__ == '__main__':
 	fn = './files/test_df.txt'
 	f = Fly(fn)
-	print(f.particles[0].splat)
-	
+	print(f.df.dtypes)
 
--- particle.py
+++ particle.py
@@ -4,12 +4,44 @@ import os
 
 
 class Particle():
-	def __init__(self, df):
-		self.__df = df
+	def __init__(self, df, zero_origin=True):
+		self.df = df
 		self.id = df['Num'].values[0]
 		self.xyz = np.array(df[['X', 'Y', 'Z']])
 		self.energy = df.iloc[0]['KE'] / 1e6 # to MeV
-		self.splat = np.array(df.loc[df['Events'] == 18, ['X', 'Y', 'Z']])
+		self.get_splat()
+		self.get_z_intercept()
+		self.calc_gradient()
+
+	def calc_gradient(self, cols=['X', 'Y', 'Z']):
+		if self.z_intercept is None:
+			self.calc_gradient_alt()
+		
+		else:
+			p2 = self.splat
+			p1 = self.z_intercept
+			diff = (p2 - p1)
+			grad = np.array([(diff[0] / diff[2]), (diff[1] / diff[2])]) * 1000
+			self.gradient = grad
+
+	def calc_gradient_alt(self):
+		self.gradient = np.nan
+
+	def get_splat(self):
+		s = np.array(self.df.loc[self.df['Events'] == 18,
+							['X', 'Y', 'Z']]).ravel()
+		self.splat = s
+
+	def get_z_intercept(self):
+		z = np.array(self.df.loc[self.df['Events'] == 4098,
+							['X', 'Y', 'Z']]).ravel()
+		if len(z) == 0:
+			z = None
+		self.z_intercept = z
+
 
 if __name__ == '__main__':
-	pass
+	from fly_parser import Fly 
+	from test_df import test_df
+	f = Fly(test_df)
+	print(f.get_splats())
